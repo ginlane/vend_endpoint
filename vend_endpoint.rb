93c4538975d03be5b4fa0c7e8ac54fe736288379
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-19 19:04:17 -0400

    vendpoint bro

--- vend_endpoint.rb
+++ vend_endpoint.rb
@@ -1,34 +1,44 @@
 class VendEndpoint < EndpointBase::Sinatra::Base
   post '/add_product' do
+    ap @options
 
     product = @payload['product']
-    ap product
     out = {
-      source_id: product['parent_id'],
-      source_variant_id: product['id'],
-      handle: product['permalink'],
+      source_id: product[:parent_id],
+      source_variant_id: product[:id],
+      handle: product[:permalink],
       type: 'Spree Product',
-      name: product['name'],
-      sku: product['sku'],
+      name: product[:name],
+      description: product[:description],
+      tags: product[:meta_keywords],
+      sku: product[:sku],
       # variant_parent_id: product.master.vend_id,
-      variant_option_one_name: product['options'].keys.first,
-      variant_option_one_value: product['options'].values.first,
-      retail_price: product['price']
+      supply_price: product[:cost_price],
+      retail_price: product[:price]
     }
-    
-    # if opt = option_values.find {|ov|ov.option_type.name == 'color'}
-    #   out[:name] = "#{product.name} / #{opt.presentation}"
-    # end
 
+    unless product[:options].blank?
+      out[:variant_option_one_name]   = product[:options].keys.first
+      out[:variant_option_one_value]  = product[:options].values.first
+      out[:name] << " - " + out[:variant_option_one_value]
+    end
 
+    vend =       Vend::Client.new(
+        'reformation',
+        'spree',
+        'spreevend8942')
+
+    resp = vend.request('products', method: :post, body: out.to_json)
+
+    ap resp
     # # Return an order object.
-    add_object :product, out
+    # add_object :vend_product, resp
 
     # # # Create or update the parameter sample.new.
     # add_parameter 'sample.new', 'butts'
 
     # # # Set the notification summary.
-    # set_summary 'The order was imported correctly'
+    set_summary 'The product was imported correctly'
 
     # # # Return a customized key and value.
     # add_value 'my_customized_key', { butt:'trucks' }
